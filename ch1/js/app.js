1717f9934cafe1ad59f3c56dc9d638d426db0a57
kirandash <thekirankumardash@gmail.com>
2016-10-30 12:24:55 +0530

    02 - 03 Syntax and First Animation

--- ch1/js/app.js
+++ ch1/js/app.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-	alert('ready');
+	startTween();
 });
+
+function startTween(){
+	// time in sec not millisec
+	// ask tweenlite to move toolBox by 2sec in horizontal direction by 50pixels
+	TweenLite.to($('#toolBox'), 1, {x: 200, y: 225});
+}
 
 /*document.addEventListener("DOMContentLoaded", function(){
 	alert('pure js ready');
