04f28f406521866524a0f7025b034ce6c34b5126
kirandash <thekirankumardash@gmail.com>
2016-10-30 15:46:12 +0530

    02 - 06 Delay the Animation

--- ch1/js/app.js
+++ ch1/js/app.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
 });
 
 function startTween(){
+	d = 1;
 	// time in sec not millisec
 	// ask tweenlite to move toolBox by 2sec in horizontal direction by 50pixels (from current location)
-	TweenLite.to($('#toolBox'), 1, {x: 290, y: 225, scale: 1.5, onComplete: returnToNormal, onCompleteParams: [$('#toolBox')]});
+	TweenLite.to($('#toolBox'), 1, {delay: d,x: 290, y: 225, scale: 1.5, onComplete: returnToNormal, onCompleteParams: [$('#toolBox')]});
 
-	TweenLite.to($('.asapReg'), 1, {color: "#ff0000", fontSize: "+=75", onComplete: returnTextToNormal});
+	TweenLite.to($('.asapReg'), 1, {delay: (d+.5),color: "#ff0000", fontSize: "+=75", onComplete: returnTextToNormal, onCompleteParams: [d]});
 
 	// fontSize: "+=150" += for relative adding - better than % add - also note that css properties are camelcase
 }
@@ -18,8 +19,8 @@ function returnToNormal(obj){
 	}
 }
 
-function returnTextToNormal(){
-	TweenLite.to($('.asapReg'), 1, {color: "#fff", fontSize: "-=75"});
+function returnTextToNormal(delay){
+	TweenLite.to($('.asapReg'), 1, {delay: delay, color: "#fff", fontSize: "-=75"});
 }
 
 /*document.addEventListener("DOMContentLoaded", function(){
