3b0a55db385b416d94ea1dd24305eb02ca366098
kirandash <thekirankumardash@gmail.com>
2016-10-30 12:54:34 +0530

    02 - 04 Add more animation

--- ch1/js/app.js
+++ ch1/js/app.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
 function startTween(){
 	// time in sec not millisec
 	// ask tweenlite to move toolBox by 2sec in horizontal direction by 50pixels
-	TweenLite.to($('#toolBox'), 1, {x: 200, y: 225});
+	TweenLite.to($('#toolBox'), 1, {x: 200, y: 225, scale: 1.5});
+
+	TweenLite.to($('.asapReg'), 1, {color: "#ff0000", fontSize: "+=75"});
+
+	// fontSize: "+=150" += for relative adding - better than % add - also note that css properties are camelcase
 }
 
 /*document.addEventListener("DOMContentLoaded", function(){
