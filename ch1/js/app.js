35d2f1b1a44185c9a9ce61dc853be01b2012a646
kirandash <thekirankumardash@gmail.com>
2016-10-30 13:25:14 +0530

    02 - 05 Animation Complete Function

--- ch1/js/app.js
+++ ch1/js/app.js
@@ -4,12 +4,22 @@ $(document).ready(function(){
 
 function startTween(){
 	// time in sec not millisec
-	// ask tweenlite to move toolBox by 2sec in horizontal direction by 50pixels
-	TweenLite.to($('#toolBox'), 1, {x: 200, y: 225, scale: 1.5});
+	// ask tweenlite to move toolBox by 2sec in horizontal direction by 50pixels (from current location)
+	TweenLite.to($('#toolBox'), 1, {x: 290, y: 225, scale: 1.5, onComplete: returnToNormal, onCompleteParams: [$('#toolBox')]});
 
-	TweenLite.to($('.asapReg'), 1, {color: "#ff0000", fontSize: "+=75"});
+	TweenLite.to($('.asapReg'), 1, {color: "#ff0000", fontSize: "+=75", onComplete: returnTextToNormal});
 
 	// fontSize: "+=150" += for relative adding - better than % add - also note that css properties are camelcase
+}
+
+function returnToNormal(obj){
+	if(obj){
+		TweenLite.to(obj, 1, {x: 0, y: 0, scale: 1});	
+	}
+}
+
+function returnTextToNormal(){
+	TweenLite.to($('.asapReg'), 1, {color: "#fff", fontSize: "-=75"});
 }
 
 /*document.addEventListener("DOMContentLoaded", function(){
