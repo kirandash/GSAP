aaa051f5a6f5d313001caafc61cdd8b8d2c33cfb
kirandash <thekirankumardash@gmail.com>
2016-10-31 21:08:58 +0530

    04 - 01 Use TweenMax and New Syntax

--- ch2-timeline/js/app.js
+++ ch2-timeline/js/app.js
@@ -5,28 +5,12 @@ $(document).ready(function(){
 
 function init(){
 	// New instance of TimelineLite
-	var toolTimeline = new TimelineLite({paused: true});
+	var toolTimeline = new TimelineMax();
 	// paused: true in the timeline constructor makes sure that the timeline is paused in the beginning
 	var duration = .5;
 	// Add tween sequence to the timeline
-	toolTimeline.add(TweenLite.to('#toolBox', duration, {y: -100, ease: Linear.easeInOut}));
-	toolTimeline.add(TweenLite.to('#weightLifter', duration, {y: -100, ease: Bounce.easeInOut}));
-	toolTimeline.add(TweenLite.to('#crazy', duration, {y: -100, ease: Elastic.easeInOut}));
-	toolTimeline.add(TweenLite.to('#leaf', duration, {y: -100, ease: Back.easeInOut}));
-
-	$('#start').click(function(){
-		toolTimeline.play();
-	});
-
-	$('#pause').click(function(){
-		toolTimeline.pause();
-	});
-
-	$('#stop').click(function(){
-		toolTimeline.stop();
-	});
-
-	$('#reverse').click(function(){
-		toolTimeline.reverse();
-	});	
+	toolTimeline.to('#toolBox', duration, {y: -100, ease: Linear.easeInOut});
+	toolTimeline.to('#weightLifter', duration, {y: -100, ease: Bounce.easeInOut});
+	toolTimeline.to('#crazy', duration, {y: -100, ease: Elastic.easeInOut});
+	toolTimeline.to('#leaf', duration, {y: -100, ease: Back.easeInOut});
 }
