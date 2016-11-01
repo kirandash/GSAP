f5b16b6790938206d8f7b2b3bd6e9abe1481a689
kirandash <thekirankumardash@gmail.com>
2016-11-01 21:35:21 +0530

    05 - 04 Add ThrowProps to Draggable

--- ch5-draggable/app/app.js
+++ ch5-draggable/app/app.js
@@ -4,10 +4,11 @@ function initProject (){
 	// Applying a dragging instance on top of everything with .moveAble class
 	Draggable.create(".moveAble", {
 		bounds: gridContainer,
-		edgeResistance: 1,
-		dragResistance: .75,
+		edgeResistance: 0.65,
+		//dragResistance: .75,
 		cursor: "pointer",
-		type: "X"
+		type: "x,y",
+		throwProps: true
 		// edgeResistance 0 to 1 - 1 can't move outside at all
 		// dragResistance 0 to 1 - 1 can't drag at all
 	})
