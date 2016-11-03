d3e06976f32f3b1ef282efbd495aec4bf9fdbbc5
kirandash <thekirankumardash@gmail.com>
2016-11-03 08:47:45 +0530

    07 - 03 Add the Final Touches

--- ch6-draggame/app/app.js
+++ ch6-draggame/app/app.js
@@ -2,6 +2,7 @@
 TweenMax.set(".image", {scale: 0, opacity: 0});
 TweenMax.set(".title", {perspective: 400, textShadow: "2px 2px 15px rgba(0,0,0,.6)"});
 // TweenMax.set(".instruction", {textShadow: "2px 2px 15px rgba(0,0,0,.6)"});
+TweenMax.set(".tryAgain", {scale: 0});
 
 var titleText = new SplitText(".title"),
 	tl = new TimelineLite({onComplete: loadGameBoard}),
@@ -58,9 +59,17 @@ function initDraggableItem() {
 
 					// hit test
 					if(dragThing.hitTest(spot, "10%")) {
+						TweenMax.to(".correct", .3, {scale: 2, autoAlpha: 1, onComplete: function(){
+							// Hide tryagain message after .2s
+							TweenMax.to(".correct", .2, {scale:0, autoAlpha: 0, delay: .5});
+						}});
 						// if our through hits the 10% of spot
-						TweenMax.to(dragThing.target, .5, {x: diffLeft, y: diffTop});
+						TweenMax.to(dragThing.target, .5, {x: diffLeft, y: diffTop, onComplete: hideMatches, onCompleteParams: [dragThing, spot] });
 					}else{
+						TweenMax.to(".tryAgain", .3, {scale: 2, autoAlpha: 1, onComplete: function(){
+							// Hide tryagain message after .2s
+							TweenMax.to(".tryAgain", .2, {scale:0, autoAlpha: 0, delay: .5});
+						}});
 						TweenMax.to(dragThing.target, .5, {x: dragThing.startX, y: dragThing.startY});
 						// at the end of drag return back to starting position
 					}
@@ -69,6 +78,17 @@ function initDraggableItem() {
 			
 		}
 	})
+}
+
+function hideMatches(dragItem, targetItem) {
+	totalHit++; // Increase the total number of hits
+	TweenMax.to([dragItem.target, targetItem], .5, {autoAlpha: 0, onComplete: checkTargetCount});
+}
+
+function checkTargetCount() {
+	if(totalHit == totalTargets){
+		TweenLite.to(".modal", .3, {autoAlpha: 1});
+	}
 }
 
 initTitle();
